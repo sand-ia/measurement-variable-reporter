189efd1e2b021475446dec22c721d394e5845038
mikevive <mikevive327@gmail.com>
2022-09-09 16:10:26 -0500

    chore(test): remove unused test
    The test_app was unused since the start up of the project is not longer being tested.

--- tests/test_app.py
+++
@@ -1,9 +0,0 @@
-"""Test Measurement Variable Reporter startup."""
-from flask import Flask
-from src import create_app
-
-
-def test_create_app() -> None:
-    """Test Flask Application Startup."""
-    app: Flask = create_app()
-    assert isinstance(app, Flask)
