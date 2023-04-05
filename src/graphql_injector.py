72040e522afbb78907b6df1f016ee80577a1e463
mikevive <mikevive327@gmail.com>
2023-04-05 15:23:12 -0500

    backup

--- src/graphql_injector.py
+++ src/graphql_injector.py
@@ -1,27 +1,25 @@
-from typing import Any, Iterable, Callable, Union
+from typing import Any, Callable, List
 from flask_graphql import GraphQLView
 from graphene import Schema
-from injector import Binder, Injector, Module
+from injector import Binder, Injector
 
-_ModuleT = Union[Callable[[Binder], Any], Module]
+_ModuleT = Callable[[Binder], None]
 
 
-def create_injected_graphql_view(
-    schema: Schema, modules: Iterable[_ModuleT] = []
-) -> Callable[..., Any]:
+def create_injected_graphql_view(schema: Schema, modules: List[_ModuleT] = []) -> Any:
     injector = Injector()
     installed_injector = _install_modules(injector, modules)
     graphql_view = _get_graphql_view(installed_injector, schema)
     return graphql_view
 
 
-def _install_modules(injector: Injector, modules: Iterable[_ModuleT] = []) -> Injector:
+def _install_modules(injector: Injector, modules: List[_ModuleT] = []) -> Injector:
     for module in modules:
         injector.binder.install(module)
     return injector
 
 
-def _get_graphql_view(injector: Injector, schema: Schema) -> Callable[..., Any]:
+def _get_graphql_view(injector: Injector, schema: Schema) -> Any:
     graphql_scheme = schema
 
     class GraphQLViewWithContext(GraphQLView):
@@ -30,5 +28,5 @@ def _get_graphql_view(injector: Injector, schema: Schema) -> Callable[..., Any]:
         def get_context(self):
             return {"dependency_container": injector}
 
-    graphql_view: Callable[..., Any] = GraphQLViewWithContext.as_view("graphql")
+    graphql_view: Any = GraphQLViewWithContext.as_view("graphql")
     return graphql_view
